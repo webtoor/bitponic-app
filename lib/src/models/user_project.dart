88163403b630f47916702f2dd9fc488baf66e158
webtoor <fityanalimunshi@gmail.com>
2020-04-11 01:10:59 +0700

    Parsing projectDevice array object

--- lib/src/models/user_project.dart
+++ lib/src/models/user_project.dart
@@ -1,29 +1,56 @@
-
-/* import 'dart:convert';
-
+/* 
 userProjectFromJson(String str) {
     final jsonData = json.decode(str);
     return new List<UserProject>.from(jsonData.map((x) => UserProject.fromJson(x)));
 }  */
 
+import 'dart:convert';
+
 class UserProject {
-    int id;
-    String projectName;
-    String projectType;
-    List projectDevice;
+  int id;
+  String projectName;
+  String projectType;
+  List<ProjectDevice> projectDevice;
 
-    UserProject({
-        this.id,
-        this.projectName,
-        this.projectType,
-        this.projectDevice,
-    });
+  UserProject({
+    this.id,
+    this.projectName,
+    this.projectType,
+    this.projectDevice,
+  });
 
-    factory UserProject.fromJson(Map<String, dynamic> json) => new UserProject(
-        id: json["data"]["id"],
-        projectName: json["data"]["project_name"],
-        projectType: json["data"]["project"]["name"],
-        projectDevice :  json["data"]["project_device"]["id"]
-    );
+  factory UserProject.fromJson(Map<String, dynamic> parsedJson) {
+    var list = parsedJson["data"]["project_device"] as List;
+    print(list);
+    List<ProjectDevice> dataList =
+        list.map((i) => ProjectDevice.fromJson(i)).toList();
 
+    return UserProject(
+        id: parsedJson["data"]["id"],
+        projectName: parsedJson["data"]["project_name"],
+        projectType: parsedJson["data"]["project"]["name"],
+        projectDevice: dataList);
+  }
+}
+
+class ProjectDevice {
+  int id;
+  int userProjectId;
+  int deviceId;
+  String key;
+
+  ProjectDevice({
+    this.id,
+    this.userProjectId,
+    this.deviceId,
+    this.key,
+  });
+
+  factory ProjectDevice.fromJson(Map<String, dynamic> parsedJson) =>
+      new ProjectDevice(
+        id: parsedJson["id"],
+        userProjectId: parsedJson["user_project_id"],
+        deviceId: parsedJson["device_id"],
+        key: parsedJson["key"],
+      );
 }
--- lib/src/screens/dashboard.dart
+++ lib/src/screens/dashboard.dart
@@ -1,4 +1,4 @@
-import 'package:bitponic/src/blocs/dashboard_bloc.dart';
+/* import 'package:bitponic/src/blocs/dashboard_bloc.dart'; */
 import 'package:bitponic/src/models/user_project.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -22,7 +22,6 @@ Future<UserProject> getUserProject() async {
         "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMWYyMGJiZWVmNGNkNTkxMTEzZmU4NTBjODUyNzA2ODZlMWU1ZjA3YTRmZmFlNjNjYTE1NGMzOWNjOTI0NTNhYzU0MzEzMTc2ZWJmMTFlMDYiLCJpYXQiOjE1ODU4MzMxNzgsIm5iZiI6MTU4NTgzMzE3OCwiZXhwIjoxNjE3MzY5MTc4LCJzdWIiOiIxIiwic2NvcGVzIjpbIm1lbWJlciJdfQ.igRML7KCPfdGcK3czP6jUJ0k0NZ4Q3zEx580qRqqoxhGxCiQOcAZKBvLN_BtHnXjAm3rhCiP369NcROWsQLiMp61Fc5NwGP2pSdD6DalLD6JuDPvxTyLapPUTEt2Iy43hIW0jMhkc8MTpAA9XiuQFpFZ4cop0G-_csG2Cy11D7OLUViIYqNevBkC76A2yhrs7TNd909JcL3EgcujguyFuwBpEoM_Snheib0qKQ1iw_tFjraiSyIldQ2QhZeaamRl8L58jQ8crXDCl9fcQnR7Yxnd55PkGoS5O7K_rSEka7Uly3PRQ7xT6aCrTLNjzo8D3tO2xk1HaZ2bgCW5Yi4tCgpgi1bW9CVgeu8-Pi6wPXxtBrvL-GmteaYFx4HGFpNOwijEnCXsYcVyGkkhND2VDrb79Y8rt-VGJui0UoBfB_HQ6kEdGxgOzkHqJBVz6y6rbR0Va6SNBD3VJgkawt_BHTROq2V-ljszQWm9giid0ZGpiYl_LDwsGB8iHL2yvqQXTrujrFXl4CzuRJtBma-T-WU_KnxZ3_-nxa1bE4GtHStoFZkpf40Q0ixDvmKmYT8lK_GoadksrMe_phO_esMlPy3Rmpc4wmUEf3Zm_kAkB9hKZLd9eXn38xo3x2IgerYOiz6ZszQDCDac9VjLQ4_4rQoq0NZH6kJSI2uYW9yz-yo"
   });
   if (response.statusCode == 200) {
-    print(response.body);
     return UserProject.fromJson(json.decode(response.body));
   } else {
     throw Exception('Failed to Load');
@@ -37,6 +36,7 @@ class DashboardState extends State<Dashboard> {
     /*  bloc.getUserProject(); */
     super.initState();
     futureUserProject = getUserProject();
+    print(UserProject);
   }
 
   @override
